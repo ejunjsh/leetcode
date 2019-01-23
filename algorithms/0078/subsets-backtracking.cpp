44c88f8595da98a9812c7775ccab293a054af6d7
zhouff <sjj050121014@163.com>
2019-01-24 00:21:50 +0800

    subsets

--- algorithms/0078/subsets-backtracking.cpp
+++ algorithms/0078/subsets-backtracking.cpp
@@ -1,14 +1,18 @@
-class Solution {
-public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> subs(1, vector<int>());
-        for (int i = 0; i < nums.size(); i++) {
-            int n = subs.size();
-            for (int j = 0; j < n; j++) {
-                subs.push_back(subs[j]); 
-                subs.back().push_back(nums[i]);
+class Solution{
+    public:
+        vector<vector<int>> subsets(vector<int>& nums){
+            vector<vector<int>> subs;
+            vector<int> sub;
+            subsets(nums, 0, sub, subs);
+            return subs;
+        }
+    private:
+        void subsets(vector<int>& nums, int start, vector<int>& sub, vector<vector<int>>& subs) {
+            subs.push_back(sub);
+            for (int i = start; i < nums.size(); i++) {
+                sub.push_back(nums[i]);
+                subsets(nums, i + 1, sub, subs);
+                sub.pop_back();
             }
         }
-        return subs;
-    }
-}; 
+};
