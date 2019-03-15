a7b30a1635a099fce5785afa24db291f685e5552
zhouff <sjj050121014@163.com>
2019-03-16 01:13:38 +0800

    Two Sum III - Data structure design

---
+++ algorithms/0170/twoSum.cpp
@@ -0,0 +1,17 @@
+class TwoSum {
+public:
+    void add(int number) {
+        ++m[number];
+    }
+    bool find(int value) {
+        for (auto a : m) {
+            int t = value - a.first;
+            if ((t != a.first && m.count(t)) || (t == a.first && a.second > 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+private:
+    unordered_map<int, int> m;
+};
