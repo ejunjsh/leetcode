84892269c1f32849f6cffb301b95567c82ceea3e
ejunjsh <sjj050121014@163.com>
2019-06-03 11:29:32 +0800

    Best Meeting Point

---
+++ algorithms/0296/minTotalDistance.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minTotalDistance(vector<vector<int>>& grid) {
+        vector<int> rows, cols;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[i].size(); ++j) {
+                if (grid[i][j] == 1) {
+                    rows.push_back(i);
+                    cols.push_back(j);
+                }
+            }
+        }
+        return minTotalDistance(rows) + minTotalDistance(cols);
+    }
+    int minTotalDistance(vector<int> v) {
+        int res = 0;
+        sort(v.begin(), v.end());
+        int i = 0, j = v.size() - 1;
+        while (i < j) res += v[j--] - v[i++];
+        return res;
+    }
+};
