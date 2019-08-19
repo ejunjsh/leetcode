53a3e867c1a3f09db72b0626a9c0dafa0ae5b2dd
ejunjsh <sjj050121014@163.com>
2019-08-19 16:57:28 +0800

    Combination Sum IV

---
+++ algorithms/0377/combinationSum4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<unsigned int> dp(target + 1);
+        dp[0] = 1;
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i <= target; ++i) {
+            for (auto a : nums) {
+                if (i < a) break;
+                dp[i] += dp[i - a];
+            }
+        }
+        return dp.back();
+    }
+};
