9b80d3bf6fcb89bb7cf3fd8d7be6ecfe3a7c3bb5
ejunjsh <sjj050121014@163.com>
2019-10-26 21:02:41 +0800

    Add Two Numbers II

--- README.md
+++ README.md
@@ -438,6 +438,7 @@ __💰 means these problems are available for subscription user__
 |442|[Find All Duplicates in an Array](https://leetcode.com/problems/find-all-duplicates-in-an-array/)|[C++](https://github.com/ejunjsh/leetcode/blob/master/algorithms/0442/findDuplicates.cpp)|Medium|
 |443|[String Compression](https://leetcode.com/problems/string-compression/)|[C++](https://github.com/ejunjsh/leetcode/blob/master/algorithms/0443/compress.cpp)|Easy|
 |444|[💰 Sequence Reconstruction](https://github.com/ejunjsh/leetcode/blob/master/algorithms/0444/README.md)|[C++](https://github.com/ejunjsh/leetcode/blob/master/algorithms/0444/sequenceReconstruction.cpp)|Medium|
+|445|[Add Two Numbers II](https://leetcode.com/problems/add-two-numbers-ii/)|[C++](https://github.com/ejunjsh/leetcode/blob/master/algorithms/0445/addTwoNumbers.cpp)|Medium|
 
 ## sql
 
---
+++ algorithms/0445/addTwoNumbers.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1, s2;
+        while (l1) {
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+        while (l2) {
+            s2.push(l2->val);
+            l2 = l2->next;
+        }
+        int sum = 0;
+        ListNode *res = new ListNode(0);
+        while (!s1.empty() || !s2.empty()) {
+            if (!s1.empty()) {sum += s1.top(); s1.pop();}
+            if (!s2.empty()) {sum += s2.top(); s2.pop();}
+            res->val = sum % 10;
+            ListNode *head = new ListNode(sum / 10);
+            head->next = res;
+            res = head;
+            sum /= 10;
+        }
+        return res->val == 0 ? res->next : res;
+    }
+};
