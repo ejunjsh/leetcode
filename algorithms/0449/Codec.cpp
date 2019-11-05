54e8aef869baf7be4d178056fe5804d2afee598f
ejunjsh <sjj050121014@163.com>
2019-11-05 17:55:15 +0800

    Serialize and Deserialize BST

---
+++ algorithms/0449/Codec.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string order;
+        preorderDFS(root, order);
+        return order;
+    }
+    
+    inline void preorderDFS(TreeNode* root, string& order) {
+        if (!root) return;
+        char buf[4];
+        memcpy(buf, &(root->val), sizeof(int)); //burn the int into 4 chars
+        for (int i=0; i<4; i++) order.push_back(buf[i]);
+        preorderDFS(root->left, order);
+        preorderDFS(root->right, order);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        int pos = 0;
+        return reconstruct(data, pos, INT_MIN, INT_MAX);
+    }
+    
+    inline TreeNode* reconstruct(const string& buffer, int& pos, int minValue, int maxValue) {
+        if (pos >= buffer.size()) return NULL; //using pos to check whether buffer ends is better than using char* directly.
+        
+        int value;
+        memcpy(&value, &buffer[pos], sizeof(int));
+        if (value < minValue || value > maxValue) return NULL;
+        
+        TreeNode* node = new TreeNode(value);
+        pos += sizeof(int);
+        node->left = reconstruct(buffer, pos, minValue, value);
+        node->right = reconstruct(buffer, pos, value, maxValue);
+        return node;
+    }
+};
