685ab83493980c971de1b7d1e7cc18e097678934
zhouff <sjj050121014@163.com>
2020-01-27 22:46:18 +0800

    Target Sum

---
+++ algorithms/0494/findTargetSumWays-dp.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        vector<vector<int>> dp(nums.size(),vector<int>(2001,0));
+        dp[0][nums[0] + 1000] = 1;
+        dp[0][-nums[0] + 1000] += 1;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int sum = -1000; sum <= 1000; sum++) {
+                if (dp[i - 1][sum + 1000] > 0) {
+                    dp[i][sum + nums[i] + 1000] += dp[i - 1][sum + 1000];
+                    dp[i][sum - nums[i] + 1000] += dp[i - 1][sum + 1000];
+                }
+            }
+        }
+        return S > 1000 ? 0 : dp[nums.size() - 1][S + 1000];
+    }
+};
