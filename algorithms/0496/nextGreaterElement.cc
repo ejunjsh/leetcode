08d429a45c222d09658e50ffbbfb82e84dcca9cd
zhouff <sjj050121014@163.com>
2020-02-03 21:16:40 +0800

    Next Greater Element I

---
+++ algorithms/0496/nextGreaterElement.cc
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> s;
+        unordered_map<int, int> m;
+        for (int n : nums2) {
+            while (s.size() && s.top() < n) {
+                m[s.top()] = n;
+                s.pop();
+            }
+            s.push(n);
+        }
+        vector<int> res;
+        for (int n : nums1) res.push_back(m.count(n) ? m[n] : -1);
+        return res;
+    }
+};
