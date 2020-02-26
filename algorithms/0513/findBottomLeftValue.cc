ea0c47ae4cffd33d878a877ff8c64d164f08548b
zhouff <sjj050121014@163.com>
2020-02-26 16:44:52 +0800

    Find Bottom Left Tree Value

---
+++ algorithms/0513/findBottomLeftValue.cc
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q{{root}};
+        while (!q.empty()) {
+            root = q.front(); q.pop();
+            if (root->right) q.push(root->right);
+            if (root->left) q.push(root->left);
+        }
+        return root->val;
+    }
+};
