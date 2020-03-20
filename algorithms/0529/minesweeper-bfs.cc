5bc6419bae64411bd863f1f5bcf96ac626872fe3
ejunjsh <sjj050121014@163.com>
2020-03-20 17:31:18 +0800

    Minesweeper

---
+++ algorithms/0529/minesweeper-bfs.cc
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
+        int m = board.size(), n = board[0].size();
+        queue<vector<int>> q;
+        q.push(click);
+        
+        while (!q.empty()) {
+            int row = q.front()[0], col = q.front()[1]; q.pop();
+            
+            if (board[row][col] == 'M') { // Mine
+                board[row][col] = 'X';
+            }
+            else { // Empty
+                // Get number of mines first.
+                int count = 0;
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (i == 0 && j == 0) continue;
+                        int r = row + i, c = col + j;
+                        if (r < 0 || r >= m || c < 0 || c < 0 || c >= n) continue;
+                        if (board[r][c] == 'M' || board[r][c] == 'X') count++;
+                    }
+                }
+                
+                if (count > 0) { // If it is not a 'B', stop further BFS.
+                    board[row][col] = count + '0';
+                }
+                else { // Continue BFS to adjacent cells.
+                    board[row][col] = 'B';
+                    for (int i = -1; i < 2; i++) {
+                        for (int j = -1; j < 2; j++) {
+                            if (i == 0 && j == 0) continue;
+                            int r = row + i, c = col + j;
+                            if (r < 0 || r >= m || c < 0 || c < 0 || c >= n) continue;
+                            if (board[r][c] == 'E') {
+                                vector<int> nextPos{r, c};
+                                q.push(nextPos);
+                                board[r][c] = 'B'; // Avoid to be added again.
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return board;
+    }
+};
