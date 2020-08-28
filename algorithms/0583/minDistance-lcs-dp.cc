0b8e361e1c61f43d61001ec4b06c2fa06189f0dc
ejunjsh <sjj050121014@163.com>
2020-08-28 15:07:03 +0800

    Delete Operation for Two Strings

--- algorithms/0583/minDistance-lcs-dp.cc
+++ algorithms/0583/minDistance-lcs-dp.cc
@@ -5,13 +5,13 @@ public:
         for (int i = 0; i <= word1.size(); i++) {
             for (int j = 0; j <= word2.size(); j++) {
                 if (i == 0 || j == 0)
-                    dp[i][j] = i + j;
-                else if (word1[i - 1] == word2[j - 1])
-                    dp[i][j] = dp[i - 1][j - 1];
+                    continue;
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
-                    dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-        return dp[word1.size()][word2.size()];
+        return word1.size() + word2.size() - 2 * dp[word1.size()][word2.size()];
     }
 };
