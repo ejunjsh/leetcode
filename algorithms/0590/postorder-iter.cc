b0fd7d2cc82786d67d61029b451a9933e274f44e
ejunjsh <sjj050121014@163.com>
2020-09-18 18:25:28 +0800

    N-ary Tree Postorder Traversal

---
+++ algorithms/0590/postorder-iter.cc
@@ -0,0 +1,36 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        if (!root) return {};
+        vector<int> res;
+        stack<Node*> st{{root}};
+        while (!st.empty()) {
+            Node *t = st.top(); st.pop();
+            res.insert(res.begin(), t->val);
+            for (Node* child : t->children) {
+                if (child) st.push(child);
+            }
+        }
+        return res;
+    }
+};
